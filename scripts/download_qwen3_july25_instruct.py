0d930532381ba01b153bc357751675e23a4859c3
Matt Franchi <mattwfranc@gmail.com>
2025-09-25 12:51:09 -0400

    scripts sync sep 25

---
+++ scripts/download_qwen3_july25_instruct.py
@@ -0,0 +1,7 @@
+import os
+os.environ["HF_HUB_ENABLE_HF_TRANSFER"] = "1"
+from huggingface_hub import snapshot_download
+snapshot_download(
+    repo_id = "Qwen/Qwen3-30B-A3B-Instruct-2507",
+    local_dir = "/share/ju/matt/sensing-ai-risks/models/Qwen3-30B-A3B-Instruct-2507"
+)
